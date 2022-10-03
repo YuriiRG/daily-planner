12eb308c93bb377d1b12b8c5ec315613ee65c276
YuriiRG <yuriirg@tutanota.com>
2022-10-03 21:29:10 +0200

    Install Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react';
 
 export default function App() {
   const [count, setCount] = useState(0);
+
   return (
     <div className='App'>
       <h1>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount(c => c + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -17,5 +16,5 @@ export default function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
