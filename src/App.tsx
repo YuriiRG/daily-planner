b95f5da0914880e74fb60bfbe1fe9d3980a1ac66
YuriiRG <yuriirg@tutanota.com>
2022-10-03 20:38:36 +0200

    Install ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <div className='App'>
       <h1>Vite + React</h1>
