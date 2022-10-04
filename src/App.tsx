0bcfbe7f8921c0a4bd3cf8a62e08bb50e0a84874
YuriiRG <yuriirg@tutanota.com>
2022-10-04 15:58:53 +0200

    Install TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
+    <div className='text-center'>
       <h1>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
+        <button onClick={() => setCount((c) => c + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
