480566584c4aba7554d4520f9841df94dd5ff615
YuriiRG <yuriirg@tutanota.com>
2022-10-06 17:14:38 +0200

    Install Husky to run lint and prettier on commit

--- src/routes/Error.tsx
+++ src/routes/Error.tsx
@@ -1,6 +1,6 @@
 export default function Error() {
   return (
-    <div className='flex h-screen flex-col items-center justify-center text-center'>
+    <div className='h-screen flex-col items-center justify-center flex text-center'>
       <h1 className='text-4xl'>404</h1>
       <h2 className='text-xl'>Not found</h2>
     </div>
