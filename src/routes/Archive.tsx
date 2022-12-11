373621424b8446698c1927272d77d5ffdf06ccee
YuriiRG <yuriirg@tutanota.com>
2022-12-11 20:24:56 +0100

    Archive adaptation for mobile and date width align

--- src/routes/Archive.tsx
+++ src/routes/Archive.tsx
@@ -5,7 +5,7 @@ import { useMainStore } from '../store';
 export default function Archive() {
   const days = useMainStore((s) => s.days);
   return (
-    <div className='flex gap-6 m-6 flex-col px-48'>
+    <div className='flex gap-6 m-6 flex-col sm:px-20 md:px-40'>
       {
         // map on object
         Object.keys(days)
@@ -13,14 +13,16 @@ export default function Archive() {
             result.push(
               <Link key={key} className='block' to={`/archive/${key}`}>
                 <div className='bg-gray-200 p-2 rounded-md hover:bg-gray-300 transition-colors flex gap-4'>
-                  <div>
+                  <div className='basis-40'>
                     {new Date(key).toLocaleDateString('en', {
                       year: 'numeric',
                       month: 'long',
                       day: 'numeric'
                     })}
                   </div>
-                  <div>{days[key].todos.length} todos </div>
+                  <div className='basis-20'>
+                    {days[key].todos.length} todos{' '}
+                  </div>
                 </div>
               </Link>
             );
