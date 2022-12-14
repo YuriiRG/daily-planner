afb4c3f9a4cc124745afeda85720e6a27e4aa941
YuriiRG <yuriirg@tutanota.com>
2022-12-14 19:17:46 +0100

    Remove redudant code and fix a bug in store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,4 @@
 import create from 'zustand';
-import type { StoreApi } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
@@ -34,64 +33,49 @@ type Store = MainStore & MainStoreActions;
 export const initialState: MainStore = {
   days: []
 };
-
-function createStoreActions(
-  set: StoreApi<Store>['setState'],
-  get: StoreApi<Store>['getState']
-): MainStoreActions {
-  return {
-    addTodo: (dayId, newTodo) =>
-      set((s) => {
-        s.days.find((d) => d.id === dayId)?.todos.push(newTodo);
-        return s;
-      }),
-    initNewDay: (newId) =>
-      set((s) => {
-        console.log(s);
-        if (s.days.find((d) => d.id === newId) === undefined) {
-          s.days.push({ id: newId, todos: [], notes: '' });
-        }
-        return s;
-      }),
-    deleteTodo: (dayId, todoId) =>
-      set((s) => {
-        const todos = s.days.find((d) => d.id === dayId)?.todos;
-        todos?.splice(
-          todos.findIndex((t) => t.id === todoId),
-          1
-        );
-        return s;
-      }),
-    editTodo: (dayId, newTodo) =>
-      set((s) => {
-        const todos = s.days.find((d) => (d.id = dayId))?.todos;
-        const todoIndex = todos?.findIndex((t) => t.id === newTodo.id);
-        if (!todos || !todoIndex) {
-          throw new Error('Todos not found');
-        }
-        todos[todoIndex] = newTodo;
-        return s;
-      }),
-    editNotes: (dayId, newNotes) =>
-      set((s) => {
-        const day = s.days.find((d) => d.id === dayId);
-        if (!day) {
-          throw new Error('Day not found');
-        }
-        day.notes = newNotes;
-        return s;
-      }),
-    importData: (newStore) => set(newStore),
-    exportData: () => get()
-  };
-}
-
 export const useMainStore = create<Store>()(
   immer(
     persist(
       (set, get) => ({
         ...initialState,
-        ...createStoreActions(set, get)
+        addTodo: (dayId, newTodo) =>
+          set((s) => {
+            s.days.find((d) => d.id === dayId)?.todos.push(newTodo);
+          }),
+        initNewDay: (newId) =>
+          set((s) => {
+            if (s.days.find((d) => d.id === newId) === undefined) {
+              s.days.push({ id: newId, todos: [], notes: '' });
+            }
+          }),
+        deleteTodo: (dayId, todoId) =>
+          set((s) => {
+            const todos = s.days.find((d) => d.id === dayId)?.todos;
+            todos?.splice(
+              todos.findIndex((t) => t.id === todoId),
+              1
+            );
+          }),
+        editTodo: (dayId, newTodo) =>
+          set((s) => {
+            const todos = s.days.find((d) => (d.id = dayId))?.todos;
+            const todoIndex = todos?.findIndex((t) => t.id === newTodo.id);
+            if (!todos || todoIndex === undefined) {
+              console.log(todos, todoIndex);
+              throw new Error('Todos not found');
+            }
+            todos[todoIndex] = newTodo;
+          }),
+        editNotes: (dayId, newNotes) =>
+          set((s) => {
+            const day = s.days.find((d) => d.id === dayId);
+            if (!day) {
+              throw new Error('Day not found');
+            }
+            day.notes = newNotes;
+          }),
+        importData: (newStore) => set(newStore),
+        exportData: () => get()
       }),
       {
         name: 'main-storage'
