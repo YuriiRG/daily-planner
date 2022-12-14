0c8d6da66cd80e79e9cc285d53d15634996db7cd
YuriiRG <yuriirg@tutanota.com>
2022-12-14 20:20:19 +0100

    Create a workaround for ts error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   plugins: [react()],
   test: {
     globals: true,
