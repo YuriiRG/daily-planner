41e1a5296d8b5a20e00bc785b352ebc6aef0209b
YuriiRG <yuriirg@tutanota.com>
2022-12-15 15:58:09 +0100

    Upgrade Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   plugins: [react()],
   test: {
     globals: true,
